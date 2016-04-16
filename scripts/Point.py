e103b7096d6d44889e9db72cb5602ba5cc222ff8
jacobhackett <jrhackett@wpi.edu>
2016-04-16 18:17:28 -0400

    init point

---
+++ scripts/Point.py
@@ -0,0 +1,18 @@
+
+
+class Point:
+
+	## constructor for points
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+
+	## overriding the equals function so we can compare two Points
+	def __eq__(self, other):
+	    if isinstance(other, self.__class__):
+	        if self.x == other.x and self.y == other.y:
+	            return True
+	        else:
+	            return False
+	    else:
+	        return False
